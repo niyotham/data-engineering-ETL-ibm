c176d1cd694c6878ffa4cde8f1dbc8f97fa1e048
NIYOMUKIZA Thamar <66441805+niyotham@users.noreply.github.com>
2023-10-23 18:13:19 +0300

    Update kafka.sh

--- Course 08 - ETL and Data Pipelines with Shell, Airflow and Kafka/labs/kafka.sh
+++ Course 08 - ETL and Data Pipelines with Shell, Airflow and Kafka/labs/kafka.sh
@@ -2,6 +2,7 @@ bin/zookeeper-server-start.sh config/zookeeper.properties
 bin/kafka-server-start.sh config/server.properties
 
 bin/kafka-topics.sh --create --topic news --bootstrap-server localhost:9092
+bin/kafka-topics.sh --create --topic toll --bootstrap-server localhost:9092
 
 bin/kafka-console-producer.sh --topic news --bootstrap-server localhost:9092
 
